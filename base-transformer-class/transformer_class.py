43bb00898e43f6a2a4bbf6cf5e047ce63b2aaede
Jacob van der Leeuw <jvanderleeuw@email.arizona.edu>
2020-05-19 10:34:38 -0700

    got rid of debugging comments and updated algorithm

--- base-transformer-class/transformer_class.py
+++ base-transformer-class/transformer_class.py
@@ -240,10 +240,8 @@ class Transformer():
                     # Only bother to get a timestamp if we don't have one specified
                     if timestamp is None:
                         working_timestamp = __internal__.get_first_timestamp(one_file, working_timestamp)
-        if timestamp is None and working_timestamp is not None:
-            timestamp = working_timestamp
-        elif timestamp is None and working_timestamp is None:
-            timestamp = str(datetime.datetime.now())
+        if timestamp is None:
+            timestamp = working_timestamp if working_timestamp else datetime.datetime.now().isoformat()
 
         # Prepare our parameters
 
